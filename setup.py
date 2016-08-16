d8d17dc788b76df420c91434f5812daa9a2d8209
Sébastien Pierre <sebastien.pierre@gmail.com>
2016-08-16 10:48:10 -0400

    Updated setup

--- setup.py
+++ setup.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python
-# Encoding: ISO-8859-1
-# vim: tw=80 ts=4 sw=4 noet 
+# Encoding: utf8
 # -----------------------------------------------------------------------------
-# Project           :   Kiwi
+# Project           : Texto
 # -----------------------------------------------------------------------------
-# Author            :   Sebastien Pierre                <sebastien.pierre@gmail.com>
-# License           :   Revised BSD License
+# Author            : Sebastien Pierre             <sebastien.pierre@gmail.com>
+# License           : Revised BSD License
 # -----------------------------------------------------------------------------
-# Creation date     :   07-Fev-2006
-# Last mod.         :   17-Jul-2007
+# Creation date     : 07-Fev-2006
+# Last mod.         : 16-Aug-2016
 # -----------------------------------------------------------------------------
 
 import sys
-from distutils.core import setup
+from   distutils.core import setup
+sys.path.insert(0, "src") ; import texto
 
-NAME        = "Kiwi"
-VERSION     = "0.8.6"
-WEBSITE     = "http://www.ivy.fr/" + NAME.lower()
+NAME        = "texto"
+VERSION     = texto.__version__
+WEBSITE     = "http://www.github.com/sebastien/texto"
 SUMMARY     = "Intuitive, flexible structured text to markup processor"
 DESCRIPTION = """\
-Kiwi is a structured text (markup) to XML/HTML processor, designed with
-embedding in mind. Very close in syntax to the Markdown markup, Kiwi however
-supports more elements (including tables), and has a more advanced XML/HTML
-integration.
-
-When using Kiwi, you can still write XML or HTML tags within your text, and use
-the markup syntax whenever you want. Kiwi is integrated into the Tahchee
-<http://www.ivy.fr/tahchee> static web site build system.
+Texto is a structured text (markup) to XML/HTML processor, designed with
+embedding in mind. Very close in syntax to the Markdown markup, Texto
+supports more elements and can be easily extended.
 """
 
 # ------------------------------------------------------------------------------
@@ -36,36 +31,39 @@ the markup syntax whenever you want. Kiwi is integrated into the Tahchee
 # ------------------------------------------------------------------------------
 
 setup(
-    name        = NAME,
-    version     = VERSION,
-    author      = "Sebastien Pierre", author_email = "sebastien@type-z.org",
-    description = SUMMARY, long_description = DESCRIPTION,
-    license     = "Revised BSD License",
-    keywords    = "text translation, markup, markdown, web",
-    url         =  WEBSITE,
-    download_url=  WEBSITE + "/%s-%s.tar.gz" % (NAME.lower(), VERSION) ,
-    package_dir = { "": "Sources" },
-    package_data= { "kiwi": ["*.css"] },
-    packages    = ["kiwi"],
-    scripts     = ["Scripts/kiwi"],
-    classifiers = [
-      "Development Status :: 4 - Beta",
-      "Environment :: Console",
-      "Intended Audience :: Developers",
-      "Intended Audience :: Information Technology",
-      "License :: OSI Approved :: BSD License",
-      "Natural Language :: English",
-      "Topic :: Documentation",
-      "Topic :: Software Development :: Documentation",
-      "Topic :: Text Processing",
-      "Topic :: Text Processing :: Markup",
-      "Topic :: Text Processing :: Markup :: HTML",
-      "Topic :: Text Processing :: Markup :: XML",
-      "Topic :: Utilities",
-      "Operating System :: POSIX",
-      "Operating System :: Microsoft :: Windows",
-      "Programming Language :: Python",
-    ]
+	name        = NAME,
+	version     = VERSION,
+	author      = "Sébastien Pierre", author_email = "sebastien.pierre@gmail.com",
+	description = SUMMARY, long_description = DESCRIPTION,
+	license     = "Revised BSD License",
+	keywords    = "text translation, markup, markdown, web",
+	url         =  WEBSITE,
+	download_url=  WEBSITE + "/%s-%s.tar.gz" % (NAME.lower(), VERSION) ,
+	package_dir = { "": "src" },
+	packages    = [
+		"texto",
+		"texto.formats",
+		"texto.parser",
+	],
+	scripts     = ["bin/texto"],
+	classifiers = [
+	  "Development Status :: 4 - Beta",
+	  "Environment :: Console",
+	  "Intended Audience :: Developers",
+	  "Intended Audience :: Information Technology",
+	  "License :: OSI Approved :: BSD License",
+	  "Natural Language :: English",
+	  "Topic :: Documentation",
+	  "Topic :: Software Development :: Documentation",
+	  "Topic :: Text Processing",
+	  "Topic :: Text Processing :: Markup",
+	  "Topic :: Text Processing :: Markup :: HTML",
+	  "Topic :: Text Processing :: Markup :: XML",
+	  "Topic :: Utilities",
+	  "Operating System :: POSIX",
+	  "Operating System :: Microsoft :: Windows",
+	  "Programming Language :: Python",
+	]
 )
 
-# EOF
+# EOF - vim: tw=80 ts=4 sw=4 noet
